0598176cb49dc59a01b0f6c9f88e402caaaa1071
Anurag <Vermaanug1974@gmail.com>
2025-08-30 17:10:56 +0530

    feat: implement add-question route and controller logic

--- backend/src/controllers/contest.controller.js
+++ backend/src/controllers/contest.controller.js
@@ -1,5 +1,7 @@
 import ContestModal from "../model/ContestModal.js";
 import ValidateCreateContestData from "../utils/ValidateCreateContestData.js";
+import ValidateAddQuestionData from "../utils/ValidateAddQuestionData.js"
+import QuestionModel from "../model/QuestionModel.js";
 
 export const createContest = async (req, res) => {
   try {
@@ -33,11 +35,7 @@ export const createContest = async (req, res) => {
       createdBy,
     });
 
-    console.log(newContest);
-
     await newContest.save();
-
-    
 
     return res.status(201).json({
       success: true,
@@ -47,6 +45,78 @@ export const createContest = async (req, res) => {
   } catch (err) {
     res.status(500).json({
       status: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const addQuestions = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide Contest ID",
+      });
+    }
+
+    const contest = await ContestModal.findById(id);
+    if (!contest) {
+      return res.status(400).json({
+        success: false,
+        message: "Contest does not exist",
+      });
+    }
+
+    const { questions } = req.body;
+    if (!questions || !Array.isArray(questions)) {
+      return res.status(400).json({
+        success: false,
+        message: "Questions array is required",
+      });
+    }
+
+
+    if (contest.numberOfQuestions !== questions.length) {
+      return res.status(400).json({
+        success: false,
+        message: `Please provide exactly ${contest.numberOfQuestions} questions`,
+      });
+    }
+
+
+    const { isValid, errors } = ValidateAddQuestionData(req);
+    if (!isValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid data",
+        errors,
+      });
+    }
+
+
+    const newQuestions = new QuestionModel({
+      contestId: id,
+      questions,
+    });
+
+    await newQuestions.save();
+
+
+    contest.status = "ready";
+    await contest.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Questions added successfully",
+      questions,
+    });
+  } catch (error) {
+
+    return res.status(500).json({
+      success: false,
       message: "Internal Server Error",
     });
   }
--- backend/src/route/contest.route.js
+++ backend/src/route/contest.route.js
@@ -1,9 +1,10 @@
 import express from "express"
 import authMiddleWare from "../middleware/auth.middleware.js";
-import { createContest } from "../controllers/contest.controller.js"
+import { createContest, addQuestions } from "../controllers/contest.controller.js"
 
 const contestRouter = express.Router();
 
 contestRouter.post("/createContest", authMiddleWare , createContest)
+contestRouter.post("/addQuestions/:id" , authMiddleWare , addQuestions)
 
 export default contestRouter;
