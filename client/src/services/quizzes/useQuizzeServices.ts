8dddac1752da77a986e270f688d1c72d8dfcb58e
Anurag Verma <vermaanug1974@gmail.com>
2026-09-01 11:49:17 +0530

    feat: created useGetQuizQuestionsService hook

--- client/src/services/quizzes/useQuizzeServices.ts
+++ client/src/services/quizzes/useQuizzeServices.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { handleGlobalGetRequestQuery } from "../apiRequest";
 import URLS from "#src/config/constant/URLS";
 import QUERY_KEYS from "#src/config/constant/QUERY_KEYS";
+import type { QuestionInput } from "#src/Pages/QuizEditor/QuizEditor.types";
 
 export interface Quiz {
   id: string;
@@ -48,5 +49,19 @@ export const useGetSingleQuizService = ({ quizId }: { quizId: string }) => {
     services: {
       getSingleQuizService,
     },
+  };
+};
+
+
+export const useGetQuizQuestionsService = ({ quizId }: { quizId?: string }) => {
+  const getQuizQuestionsService = useQuery({
+    queryKey: [QUERY_KEYS.QUIZ_QUESTIONS, quizId],
+    queryFn: () =>
+      handleGlobalGetRequestQuery<{ questions: QuestionInput[] }>({ url: `${URLS.QUIZZES}/${quizId}/questions` }),
+    enabled: !!quizId,
+  });
+
+  return {
+    services: { getQuizQuestionsService },
   };
 };
